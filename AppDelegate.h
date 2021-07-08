e2278b509d967fbac3f7ebac458f0cd06b0e823c
zhoubingfeng <zhoubingfeng@hikvision.com.cn>
2021-07-08 18:24:39 +0800

    444

---
+++ AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by zhoubingfeng on 18.6.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
